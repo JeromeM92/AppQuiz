362b123ae075c3bcbefbe305af15718e66df2e35
JeromeM92 <jerome.marras@hotmail.fr>
2023-02-03 15:05:03 +0100

    Done screenLogin

--- app/src/main/java/com/example/appquiz/presentation/LoginActivity.kt
+++ app/src/main/java/com/example/appquiz/presentation/LoginActivity.kt
@@ -1,18 +1,42 @@
 package com.example.appquiz.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.appquiz.MainActivity
 import com.example.appquiz.R
 import com.example.appquiz.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var loginViewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
 
+        loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        loginViewModel.loginResult.observe(this, Observer { processLogin(it) })
 
+        binding.validate.setOnClickListener {
+            loginViewModel.login(
+                binding.editUserName.text.toString(),
+                binding.editPassword.text.toString()
+            )
+        }
+
+    }
+
+    private fun processLogin(isAuthenticated: Boolean) {
+        if (isAuthenticated) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }else {
+            Toast.makeText(this,  "Mauvais identifiant", Toast.LENGTH_LONG)
+        }
     }
 }
