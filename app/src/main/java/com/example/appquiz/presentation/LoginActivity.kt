8bccf2b83e9b6620ad4fdd99fa6afd7be223846d
JeromeM92 <jerome.marras@hotmail.fr>
2023-02-03 14:43:40 +0100

    Done cleanArchi

--- app/src/main/java/com/example/appquiz/presentation/LoginActivity.kt
+++ app/src/main/java/com/example/appquiz/presentation/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.example.appquiz.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appquiz.R
+import com.example.appquiz.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        
     }
 }
