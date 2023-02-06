437ed3b1155e52ca2bcbf7a7b7845b844f55d95d
JeromeM92 <jerome.marras@hotmail.fr>
2023-02-06 18:05:40 +0100

    Done RecyclerView

--- app/src/main/java/com/example/appquiz/MainActivity.kt
+++ app/src/main/java/com/example/appquiz/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.appquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.example.appquiz.presentation.QuizzFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+
+        fragmentTransaction.add(R.id.fragment_container, QuizzFragment())
+            .addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
